5e9261ed7a3c90227b3af54ba1a41fc16cd2dd9b
Charles Korn <me@charleskorn.com>
2021-01-23 18:24:01 +1100

    Update fish-completion to 0.4.0.

--- Formula/batect-fish-completion.rb
+++ Formula/batect-fish-completion.rb
@@ -1,8 +1,8 @@
 class BatectFishCompletion < Formula
   desc "Fish shell tab completion for Batect"
   homepage "https://github.com/batect/fish-completion"
-  url "https://raw.githubusercontent.com/batect/fish-completion/0.3.0/completions/batect.fish"
-  sha256 "6d5f3831a9636b984327d48bb9373b66664e60b1a4d037203f432f4c52435140"
+  url "https://raw.githubusercontent.com/batect/fish-completion/0.4.0/completions/batect.fish"
+  sha256 "0852881e453e4b309c1a388d625ee09e306a9e86731802120291b9eeded020f5"
   license "Apache-2.0"
   head "https://raw.githubusercontent.com/batect/fish-completion/master/completions/batect.fish"
 
