854a674add90b690fc0903e5ec9e91c8baba6686
Charles Korn <me@charleskorn.com>
2021-02-20 16:20:10 +1100

    Update zsh-completion to 0.2.0.

--- Formula/batect-zsh-completion.rb
+++ Formula/batect-zsh-completion.rb
@@ -1,8 +1,8 @@
 class BatectZshCompletion < Formula
   desc "Zsh shell tab completion for Batect"
   homepage "https://github.com/batect/zsh-completion"
-  url "https://raw.githubusercontent.com/batect/zsh-completion/0.1.2/completions/_batect"
-  sha256 "2ebeebab354063574fce06fe1d0902f6204e32ddb312387254276dd60eddd683"
+  url "https://raw.githubusercontent.com/batect/zsh-completion/0.2.0/completions/_batect"
+  sha256 "471dd8f2978be8e8f3860ca8e6372f586274372ef4ab0bfdca21f68c5837577b"
   license "Apache-2.0"
   head "https://raw.githubusercontent.com/batect/zsh-completion/master/completions/_batect"
 
