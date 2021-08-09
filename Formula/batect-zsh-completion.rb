cd9e0de0a262e10ea1911efac32b64b3911c5ac6
Charles Korn <me@charleskorn.com>
2021-08-09 15:15:55 +1000

    Use most recent repository names in formulae.

--- Formula/batect-zsh-completion.rb
+++ Formula/batect-zsh-completion.rb
@@ -1,10 +1,10 @@
 class BatectZshCompletion < Formula
   desc "Zsh shell tab completion for Batect"
-  homepage "https://github.com/batect/zsh-completion"
+  homepage "https://github.com/batect/batect-zsh-completion"
   url "https://raw.githubusercontent.com/batect/batect-zsh-completion/0.3.0/_batect"
   sha256 "2199b1e349c1a6d1460e9eea22e382243f14f22a4b8fb57e9be265dbab1af66b"
   license "Apache-2.0"
-  head "https://raw.githubusercontent.com/batect/zsh-completion/master/completions/_batect"
+  head "https://raw.githubusercontent.com/batect/batect-zsh-completion/master/completions/_batect"
 
   bottle :unneeded
 
